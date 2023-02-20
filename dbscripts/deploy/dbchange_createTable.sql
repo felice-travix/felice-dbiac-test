1306fa556a31b9ff8a2075857b71a1ac23a18705
fmeneses <fmeneses@travix.com>
2023-02-20 12:52:58 +0100

    added create table for test_tbl

--- dbscripts/deploy/dbchange_createTable.sql
+++ dbscripts/deploy/dbchange_createTable.sql
@@ -26,3 +26,11 @@ create table company (
 --comment: example comment
 alter table person add column country varchar(2)
 --rollback ALTER TABLE person DROP COLUMN country;
+
+create table test_tble01 (
+    id int primary key auto_increment not null,
+    name varchar(50) not null,
+    address1 varchar(50),
+    address2 varchar(50),
+    city varchar(30)
+)
